e158fb845b7e555ed3cb7d48c6a353346f41ee5d
Andrew <jiganyg@gmail.com>
2023-03-11 19:29:27 +0800

    Exit whole script when command fails

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # if [ ! -f "/usr/bin/python" ] && [ -f "/usr/bin/python3" ]; then sudo ln -s /usr/bin/python3 /usr/bin/python; fi
 
 LDFLAGS=-L/usr/lib/x86_64-linux-gnu/ CFLAGS=-static ./configure
--- pysrc/runModel.sh
+++ pysrc/runModel.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [ $# -eq 0 ]; then
     echo "Usage: ./runModel.sh <modelName> [<output path>]"
     exit 1
