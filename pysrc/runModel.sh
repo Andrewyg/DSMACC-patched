1a644e57a6a40d01d33b2e4f98033d226c0bb3c7
Andrew <jiganyg@gmail.com>
2023-03-11 18:06:15 +0800

    Create runModel.sh

---
+++ pysrc/runModel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: ./runModel.sh <modelName> [<output path>]"
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    OUTPUT="$1_output.png"
+else
+    OUTPUT="$2"
+fi
+
+python "$1.py"
+python plot.py "$1conc.dat" "$OUTPUT"
