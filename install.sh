117e38b2d42428913cb5df51134646cfe9b21ff5
Andrewyg <jiganyg@gmail.com>
2023-04-07 19:56:55 +0800

    Don't auto install/make `pysrc/`

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ LDFLAGS=-L/usr/lib/x86_64-linux-gnu/ CFLAGS=-static ./configure
 
 make "$@"
 
-cd pysrc/
-make source "$@"
-read -p "We're going to run command in `sudo`, press enter than type your password."
-sudo make install
+#cd pysrc/
+#make source "$@"
+#read -p "We're going to run command in `sudo`, press enter than type your password."
+#sudo make install
