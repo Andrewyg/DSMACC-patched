c48aa88cdbe5347d56528e1443e5e3275912e2e3
Andrew@TW <jiganyg@gmail.com>
2023-04-08 07:12:33 +0800

    Delete fixKPP.sh

--- user/fixKPP.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-vi +':e ++ff=dos' +':wq ++ff=unix' $1
-vi +':%s/^\s*CALL mcm_constants/! CALL mcm_constants/' +':wq' $1
-vi +':%s/= :/= DUMMY :/g' +':wq' $1
-vi +':%s/^ = IGNORE/DUMMY = IGNORE/' +':wq' $1
